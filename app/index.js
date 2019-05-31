2cb45819e438732e9e9f0ce0d047f49c1f59161c
Gabriel Marks <gabriel_marks@brown.edu>
2019-05-31 18:16:39 -0400

    Fix the fix

--- app/index.js
+++ app/index.js
@@ -7,15 +7,21 @@ import { display } from "display";
 messaging.peerSocket.onopen = function() {
   if (HeartRateSensor) {
     console.log("This device has a HeartRateSensor!");
-    const hrm = new HeartRateSensor({frequency: 0.2});
+    const hrm = new HeartRateSensor({frequency: 1});
     console.log("turning off app tm")
     me.appTimeoutEnabled = false;
     display.on = false;
+    var i = 1; // hack to get around min frequency of 1Hz
     hrm.addEventListener("reading", () => {
       if (me.appTimeoutEnabled) {
         console.log("turning off app tm")
         me.appTimeoutEnabled = false;
       }
+      if(i < 5) {
+        i += 1; 
+        return;
+      }
+      i = 1;
       console.log(`Current heart rate: ${hrm.heartRate}`);
       const toCompanion = {"heartrate": hrm.heartRate, "timestamp": hrm.timestamp};
       if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
